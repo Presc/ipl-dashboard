000cf8517358e3ebdb699ff13874d778ccc6f51f
Owen Prescot <oprescot@gmail.com>
2021-07-01 21:34:20 +0100

    First API created which returns matches by team

--- src/main/java/io/prescot/ipldashboard/model/Team.java
+++ src/main/java/io/prescot/ipldashboard/model/Team.java
@@ -1,9 +1,12 @@
 package io.prescot.ipldashboard.model;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Transient;
 
 @Entity
 public class Team {
@@ -14,6 +17,13 @@ public class Team {
     private String teamName;
     private long totalMatches;
     private long totalWins;
+    
+    @Transient
+    private List<Match> matches; 
+
+    public Team() {
+        
+    }
 
     public Team(String teamName, long totalMatches) {
         this.teamName = teamName;
@@ -45,10 +55,17 @@ public class Team {
         this.totalWins = totalWins;
     }
 
+    public List<Match> getMatches() {
+        return matches;
+    }
+
+    public void setMatches(List<Match> matches) {
+        this.matches = matches;
+    }
+
     @Override
     public String toString() {
         return "Team [teamName=" + teamName + ", totalMatches=" + totalMatches + ", totalWins=" + totalWins + "]";
-    }  
-    
+    }     
     
 }
